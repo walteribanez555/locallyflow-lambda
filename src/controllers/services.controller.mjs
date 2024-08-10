ed95856e60e106c7fe7b9f225ec9b2887ec7da14
walteribanez555 <walteribanez555@gmail.com>
2024-08-10 10:59:49 -0400

    post Service added

--- src/controllers/services.controller.mjs
+++ src/controllers/services.controller.mjs
@@ -1,6 +1,6 @@
 // import { getCupones } from "../models/cupones.model.mjs";
 import { buildResponse } from "../utils/helpers.mjs";
-import { getServices as getServicesModels } from "../models/services.models.mjs";
+import { getServices as getServicesModels, postServices as postServicesModels, putServices as putServicesModels, deleteServices as deleteServiceModels } from "../models/services.models.mjs";
 
 
 export async function getServices({ id } ){
@@ -15,3 +15,44 @@ export async function getServices({ id } ){
         return buildResponse(500, {message : e}, 'get')
     }
 }
+
+export async function postServices({ data }){
+    try{
+        const services = await postServicesModels({ data, schema: 'localflow' });
+
+        return buildResponse(200, services, 'post')
+
+    }catch(e) { 
+        return buildResponse(500, {message : e}, 'post')
+    }
+
+}
+
+
+export async function putServices({ data }){
+    // try{
+
+    //     const services = await putServicesModels({ data, schema: 'localflow' });
+
+    //     return buildResponse(200, services, 'put')
+
+    // }catch(e) { 
+    //     return buildResponse(500, {message : e}, 'put')
+    // }
+
+}
+
+export async function deleteServices({ id }){
+
+    // try{
+
+    //     const services = await deleteServiceModels({ id, schema: 'localflow' });
+
+    //     return buildResponse(200, services, 'delete')
+
+        
+    // }catch(e) { 
+    //     return buildResponse(500, {message : e}, 'delete')
+    // }
+
+}   
--- src/models/services.models.mjs
+++ src/models/services.models.mjs
@@ -31,3 +31,39 @@ export async function getServices({ id, schema }) {
       return buildResponse(500, error, "get");
     }
   }
+
+
+export async function postServices({ data, schema}) {
+    try{
+        const database = new DatabaseOperations( tableName, schema);
+        const newRegister = validateData(data, model);
+        if( Object.keys( newRegister ).length === 0) 
+            return buildResponse(400, {message : 'Missing required fields or not valid'}, 'post');
+
+
+        const response = await database.create(newRegister, keyField);
+        return buildResponse(200, response, 'post', keyField, data);
+
+    }catch(error) { 
+        colorLog( ` POST BLOGS ERROR:  ${ JSON.stringify( error ) }`, 'red', 'reset' );
+        return buildResponse( 500, error, 'post' );
+    }
+
+}
+
+export async function putServices() { 
+    // try{
+
+    // }catch(error) { 
+
+    // }
+}
+
+
+export async function deleteServices() { 
+    // try{
+        
+    // }catch(error) { 
+
+    // }
+}
